6a32c5af286c1573d2c481e03143ba949cf5a112
mimidots <muragemartin22@gmail.com>
2017-11-08 09:25:26 +0300

    Added TODOs Changed where function to accept any number of param Added get() function capabilities Removed latest and find functions (for now)

--- src/Builder.php
+++ src/Builder.php
@@ -18,12 +18,14 @@ class Builder {
 	protected $limit;
 	
 	public static function table( $table ) {
+		//TODO sanitize the table name
 		self::$table = $table;
 		
 		return new static;
 	}
 	
 	public function select( $columns = "" ) {
+		//TODO sanitize the columns
 		//check if an array of columns was passed(a hack)
 		$this->columns = is_array( $columns ) ? $this->columnize( $columns )
 			: $columns;
@@ -39,48 +41,61 @@ class Builder {
 	 * @return string
 	 */
 	public function columnize( array $columns ) {
+		//TODO sanitize the columns
 		return implode( ",", array_values( $columns ) );
 	}
 	
 	
-	public function where( $column, $operator = "", $value ) {
+	/**
+	 * $column, $operator = "", $value
+	 * @return $this
+	 */
+	public function where($params) {
+		//TODO sanitize the parameters
+		//TODO check if the operator is correct
 		
-		empty( $operator ) ? $this->whereby = $column . ' =' . $value :
-			$this->whereby = $operator . " {$operator} " . $value;
+		
+		if(func_num_args()==3){
+			$this->whereby = func_get_arg(0) .''
+			                 .func_get_arg(1).''
+			                 . func_get_arg(2);
+		}
+		else
+		if(func_num_args()==2) {
+			$this->whereby = func_get_arg(0) . ' = '
+			                 . func_get_arg(1) ;
+		}
+		
+		//TODO: else return an error of invalid parameters
 		
 		return $this;
 	}
 	
 	
-	public function get( $limit = 500 ) {
-		
+	public function get( $limit = "" ) {
+		//TODO check if the limit is a number
 		$table_name = self::$table;
 		
 		/* if no column passed as param, select all	 */
 		$columns = empty( $this->columns ) ? "*" : $this->columns;
 		
 		
+		
 		$query = /** @lang text */
 			"SELECT {$columns} FROM {$table_name}";
 		
-		
+			if(!empty($this->whereby)){
+				$query=$query.' WHERE '.$this->whereby;
+			}
+			
+			
+			if(!empty($limit)){
+				$query=$query.' LIMIT '.$limit;
+			}
+			
 		echo $query;
 	}
 	
-	public function latest() {
-		$this->order = "ASC";
-		
-		return $this;
-	}
-	
-	/**
-	 * get a specific value where id= ?
-	 *
-	 * @param $id
-	 */
-	public function find( $id ) {
-	
-	}
 	
 	/**
 	 * Executes a query
@@ -88,6 +103,7 @@ class Builder {
 	 * @param $sql
 	 */
 	protected function exec( $sql ) {
+		//TODO sanitize the sql query
 		try {
 		
 		} catch ( PDOException $e ) {
