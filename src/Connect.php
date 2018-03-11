b20c8cd39f6cb54a962c61d176b493429427f04b
mimidots <mimidotsuser@gmail.com>
2018-03-11 19:29:33 +0300

    Updated the copyright file Renamed the namespace Added the default success response Changed error response on database connection failure Moved the exit function(renamed to terminate) from builder class

--- src/Connect.php
+++ src/Connect.php
@@ -1,58 +1,82 @@
 <?php
 /**
  * All rights reserved.
- * User: Dread Pirate Roberts
+ * User: hello@mimidots.com
  * Date: 29-Oct-17
  * Time: 09:42
  */
 
-namespace QueryBuilder\db;
-
+namespace QueryBuilder;
 
 use Dotenv\Dotenv;
 use PDO;
 use PDOException;
 
 
-class Connect {
-	
-	private static $conn;
+class Connect
+{
 
-	protected static function makeConnection() {
-		 $dotenv = new Dotenv('.'); //load .env file from source root folder
-		 $dotenv->load();
-		 /*ENSURE THE FOLLOWING ARE PROVIDEND*/
-		$dotenv->required(['DB_HOST', 'DB_PORT', 'DB_NAME','DB_USERNAME', 'DB_PASSWORD']);
-		
-		/*CHECK TO ENSURE THERE ARE NOT EMPTY*/
-		$dotenv->required('DB_HOST')->notEmpty();
-		$dotenv->required('DB_PORT')->notEmpty();
-		$dotenv->required('DB_NAME')->notEmpty();
-		
-		/*CHECK IF PORT IS AN INTEGER*/
-		$dotenv->required('DB_PORT')->isInteger();
-		
-		
-		
-		try {
-			self::$conn = new PDO( "mysql:host=" . $_SERVER["DB_HOST"] . ':' . $_SERVER["DB_PORT"]. ';dbname=' .  $_SERVER["DB_NAME"], $_SERVER["DB_USERNAME"], $_SERVER["DB_PASSWORD"] );
-			
-			// set the PDO error mode to exception
-			self::$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-		} catch ( PDOException $e ) {
-			
-			die("Connection failed: " . $e->getCode());
-		}
-	}
-	
-	
-	/**
-	 * @return mixed
-	 */
-	public static function getConn() {
-		self::makeConnection();
-		PDO:$conn=self::$conn;
-		return $conn;
-	}
-	
+    private static $conn;
+    protected static $response =
+        array("status" => "success",
+            "response" => "success",
+            "code" => 200);
+
+    private static function makeConnection()
+    {
+        $dotenv = new Dotenv('.'); //load .env file from source root folder
+        $dotenv->load();
+
+        /*ENSURE THE FOLLOWING ARE PROVIDEND*/
+        $dotenv->required(['DB_HOST', 'DB_PORT', 'DB_NAME', 'DB_USERNAME', 'DB_PASSWORD']);
+
+        /*CHECK TO ENSURE THERE ARE NOT EMPTY*/
+        $dotenv->required('DB_HOST')->notEmpty();
+        $dotenv->required('DB_PORT')->notEmpty();
+        $dotenv->required('DB_NAME')->notEmpty();
+
+        /*CHECK IF PORT IS AN INTEGER*/
+        $dotenv->required('DB_PORT')->isInteger();
+
+
+        try {
+            self::$conn = new PDO("mysql:host=" . $_SERVER["DB_HOST"] . ':' . $_SERVER["DB_PORT"] . ';dbname=' . $_SERVER["DB_NAME"], $_SERVER["DB_USERNAME"], $_SERVER["DB_PASSWORD"]);
+
+            // set the PDO error mode to exception
+            self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+
+            self::$response['status'] = 'error';
+            self::$response['response'] = "cannot establish connection with the database.Check the provided credentials";
+            self::$response['code'] = $e->getCode();
+            self::terminate(json_encode(self::$response));
+        }
+    }
+
+
+    /**
+     * @return mixed
+     */
+    protected static function getConn()
+    {
+        self::makeConnection();
+        PDO:
+        $conn = self::$conn;
+        return $conn;
+    }
+
+    /**
+     * returns json encoded data. Used to terminate execution of statements and or return data
+     * @param $data:optional
+     *
+     * @return string
+     */
+    protected static function terminate($data = null)
+    {
+        if (static::$response["error"] != false) {
+            return json_encode(static::$response);
+        }
+        return json_encode($data);
+
+    }
 }
